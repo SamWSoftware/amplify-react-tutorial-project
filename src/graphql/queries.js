cb5fb4ff645c7616ec12f001cd276da189c1c227
Sam Williams <sam@completecoding.io>
2021-02-14 13:33:56 +0100

    setup Amplify

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -2,37 +2,33 @@
 // this is an auto generated file. This will be overwritten
 
 export const getSong = /* GraphQL */ `
-  query GetSong($id: ID!) {
-    getSong(id: $id) {
-      id
-      title
-      description
-      filePath
-      like
-      owner
-      createdAt
-      updatedAt
+    query GetSong($id: ID!) {
+        getSong(id: $id) {
+            id
+            title
+            description
+            filePath
+            likes
+            owner
+            createdAt
+            updatedAt
+        }
     }
-  }
 `;
 export const listSongs = /* GraphQL */ `
-  query ListSongs(
-    $filter: ModelSongFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listSongs(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        title
-        description
-        filePath
-        like
-        owner
-        createdAt
-        updatedAt
-      }
-      nextToken
+    query ListSongs($filter: ModelSongFilterInput, $limit: Int, $nextToken: String) {
+        listSongs(filter: $filter, limit: $limit, nextToken: $nextToken) {
+            items {
+                id
+                title
+                description
+                filePath
+                likes
+                owner
+                createdAt
+                updatedAt
+            }
+            nextToken
+        }
     }
-  }
 `;
